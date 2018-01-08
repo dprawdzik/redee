12ff732ac0accc284f7f620d83c90e196dd70ff7
dprawdzik <daniel.prawdzik@scout24.com>
2018-01-08 12:46:07 +0100

    Initial check in of pom and project structue

---
+++ redee-core/src/main/java/com/scout24/hackdays/App.java
@@ -0,0 +1,13 @@
+package com.scout24.hackdays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
