8c001eb4d3bfdd0af13f91095d990d2b91eb3fc1
Oleksii Ustenko <oleksii.ustenko@scout24.com>
2018-01-08 15:03:48 +0100

    moved exposes.json

--- redee-core/src/main/java/com/scout24/hackdays/App.java
+++ redee-core/src/main/java/com/scout24/hackdays/App.java
@@ -7,8 +7,10 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Hello world!
@@ -18,25 +20,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        Gson gson = new Gson();
 
+        List<Expose> exposes = getExposes();
+        extractKeywords(exposes);
+        System.out.println( "Hello World!");
+    }
+
+    private static List<Expose> getExposes() {
+        Gson gson = new Gson();
         try {
-            String exposesString = new String(Files.readAllBytes(Paths.get("./src/main/exposes.json")));
+            String exposesString = new String(Files.readAllBytes(Paths.get("exposes.json")));
             Type listType = new TypeToken<List<Expose>>(){}.getType();
-            List<Expose> exposes = gson.fromJson(exposesString, listType);
-            extractKeywords(exposes);
+            return gson.fromJson(exposesString, listType);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-
-        System.out.println( "Hello World!");
+        return Collections.emptyList();
     }
 
     private static void extractKeywords(List<Expose> exposes) {
         for(Expose expose :exposes) {
-            List<String> keywords = new ArrayList<>();
+            Set<String> keywords = new HashSet<>();
             keywords.addAll(KeywordExtractor.findKeywords(expose.title));
             keywords.addAll(KeywordExtractor.findKeywords(expose.descriptionNote));
             keywords.addAll(KeywordExtractor.findKeywords(expose.furnishingNote));
