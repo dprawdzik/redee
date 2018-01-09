55442bfb7bf69ba8d83fc9579a3e2ede4b63a0b5
Oleksii Ustenko <oleksii.ustenko@scout24.com>
2018-01-09 12:35:10 +0100

    added test for KeywordExtractor

---
+++ redee-core/src/main/java/com/scout24/redee/extraction/keywords/KeywordExtractor.java
@@ -0,0 +1,34 @@
+package com.scout24.redee.extraction.keywords;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class KeywordExtractor {
+
+    private final String[] availableKeywords;
+
+    public KeywordExtractor(String[] keywords) {
+        availableKeywords = keywords;
+    }
+
+    public List<String> findKeywords(String target) {
+        List<String> foundKeywords = new ArrayList<>();
+
+        if (target == null) {
+            return foundKeywords;
+        }
+
+        String preparedTarget = target.toLowerCase();
+
+        for (String keyword : availableKeywords) {
+            Pattern pattern = Pattern.compile("(?<!(kein.{0,2}|ohne)\\s)\\b" + keyword.toLowerCase());
+            if (pattern.matcher(preparedTarget).find()) {
+                foundKeywords.add(keyword);
+            }
+        }
+
+        return foundKeywords;
+    }
+
+}
--- redee-core/src/main/java/com/scout24/redee/extraction/keywords/KeywordsExtractorPlayground.java
+++ redee-core/src/main/java/com/scout24/redee/extraction/keywords/KeywordsExtractorPlayground.java
@@ -1,7 +1,8 @@
-package com.scout24.hackdays;
+package com.scout24.redee.extraction.keywords;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.scout24.redee.extraction.model.Expose;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -12,14 +13,23 @@ import java.util.List;
 import java.util.Set;
 
 public class KeywordsExtractorPlayground {
+
+    private static final String RESOURCES_PATH = "redee-core/src/main/resources/scout24/";
+
+    private static KeywordExtractor extractor;
+
     public static void main(String[] args) {
         Gson gson = new Gson();
 
         try {
-            String exposesString = new String(Files.readAllBytes(Paths.get("redee-core/src/main/resources/scout24/exposes.json")));
+            String exposesString = new String(Files.readAllBytes(Paths.get(RESOURCES_PATH + "exposes.json")));
             Type listType = new TypeToken<List<Expose>>() {
             }.getType();
             List<Expose> exposes = gson.fromJson(exposesString, listType);
+
+            String keywordsString = new String(Files.readAllBytes(Paths.get(RESOURCES_PATH + "keywords.json")));
+            extractor = new KeywordExtractor(gson.fromJson(keywordsString, String[].class));
+
             extractKeywords(exposes);
         } catch (IOException e) {
             e.printStackTrace();
@@ -27,7 +37,6 @@ public class KeywordsExtractorPlayground {
     }
 
     private static void extractKeywords(List<Expose> exposes) {
-        KeywordExtractor extractor = new KeywordExtractor();
         for (Expose expose : exposes) {
             Set<String> keywords = new HashSet<>();
             keywords.addAll(extractor.findKeywords(expose.title));
@@ -36,16 +45,8 @@ public class KeywordsExtractorPlayground {
             keywords.addAll(extractor.findKeywords(expose.locationNote));
             keywords.addAll(extractor.findKeywords(expose.otherNote));
 
-            Set<String> keywordsRegex = new HashSet<>();
-            keywordsRegex.addAll(extractor.findKeywordsRegex(expose.title));
-            keywordsRegex.addAll(extractor.findKeywordsRegex(expose.descriptionNote));
-            keywordsRegex.addAll(extractor.findKeywordsRegex(expose.furnishingNote));
-            keywordsRegex.addAll(extractor.findKeywordsRegex(expose.locationNote));
-            keywordsRegex.addAll(extractor.findKeywordsRegex(expose.otherNote));
-
             System.out.println("Expose id: " + expose.id);
             System.out.println("Keywords     : " + keywords);
-            System.out.println("KeywordsRegex: " + keywordsRegex);
         }
     }
 }
